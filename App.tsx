6b9a638f38bfa8d5327a380f934f19e3f32757ce
Carlos Oliveira <carlos.oliverhp@gmail.com>
2023-09-15 22:18:32 -0300

    native base added to project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/screens/routes/Router';
+import { NativeBaseProvider } from "native-base";
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Router />
+      <NativeBaseProvider>
+        <Router />
+      </NativeBaseProvider> 
     </NavigationContainer>
   )
 }
