3a4f24d6c04a0afb9c3efe20f2ceea06c351010c
Carlos Oliveira <carlos.oliverhp@gmail.com>
2023-09-16 13:01:07 -0300

    search component layout

--- src/components/SearchComponent/SearchComponent.styles.ts
+++ src/components/SearchComponent/SearchComponent.styles.ts
@@ -2,9 +2,9 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   iconStyle: {
-    color: 'white',
-    fontSize: 30,
-    paddingRight: 10,
+    backgroundColor: "white",
+    padding: 5,
+    borderRadius: 50
   }
 });
 
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,14 @@
-import { View} from 'react-native';
+import { SafeAreaView} from 'react-native';
 import React, { type FunctionComponent } from 'react';
 import { Header } from '../../components/Header';
+import { SearchComponent } from '../../components/SearchComponent';
 
 const Home: FunctionComponent = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Header />
-    </View>
+      <SearchComponent />
+    </SafeAreaView>
   )
 }
 
